b61f76b34978f6a8d5b84e1017f4e47434863ce7
Ian <iamian@stclab.com>
2026-03-10 10:01:07 +0900

    Use gh CLI download strategy for private release assets
    Custom GitHubCliDownloadStrategy uses `gh release download` to
    authenticate against private repo releases.

--- Formula/amaranth-check.rb
+++ Formula/amaranth-check.rb
@@ -1,20 +1,49 @@
+require "download_strategy"
+
+class GitHubCliDownloadStrategy < CurlDownloadStrategy
+  def initialize(url, name, version, **meta)
+    super
+    match_data = %r{^https?://github\.com/(?<owner>[^/]+)/(?<repo>[^/]+)/releases/download/(?<tag>[^/]+)}.match(@url)
+    return unless match_data
+
+    @owner = match_data[:owner]
+    @repo = match_data[:repo]
+    @tag = match_data[:tag]
+    @filename = File.basename(@url)
+  end
+
+  def fetch(timeout: nil)
+    ohai "Downloading #{url} using GitHub CLI"
+    if cached_location.exist?
+      puts "Already downloaded: #{cached_location}"
+    else
+      temporary_path.dirname.mkpath
+      system_command!("gh", args: [
+        "release", "download", @tag,
+        "-R", "#{@owner}/#{@repo}",
+        "--pattern", @filename,
+        "-D", temporary_path.to_s,
+      ], print_stderr: true)
+
+      downloaded_file = Dir["#{temporary_path}/*"].first
+      raise CurlDownloadStrategyError, "Downloaded file not found" unless downloaded_file
+
+      cached_location.dirname.mkpath
+      FileUtils.mv(downloaded_file, cached_location)
+    end
+    symlink_location.dirname.mkpath
+    FileUtils.ln_s cached_location.relative_path_from(symlink_location.dirname), symlink_location, force: true
+  end
+end
+
 class AmaranthCheck < Formula
   desc "macOS menu bar app for Amaranth attendance tracking"
   homepage "https://github.com/STCLab-Inc/amaranth-check"
-  version "0.3.0"
+  url "https://github.com/STCLab-Inc/amaranth-check/releases/download/v0.3.0/amaranth-check-0.3.0-arm64.tar.gz",
+      using: GitHubCliDownloadStrategy
   sha256 "eb473e6bb2db4a4523b8d374183736223e67eb678b323399f7165f223523aed2"
+  version "0.3.0"
   license "Proprietary"
-
-  url do
-    assets = GitHub.get_release("STCLab-Inc", "amaranth-check", "v#{version}")
-                   .fetch("assets")
-    asset = assets.find { |a| a["name"] == "amaranth-check-#{version}-arm64.tar.gz" }
-                  .fetch("url")
-    [asset, header: [
-      "Accept: application/octet-stream",
-      "Authorization: bearer #{GitHub::API.credentials}",
-    ]]
-  end
 
   depends_on "node"
   depends_on :macos
