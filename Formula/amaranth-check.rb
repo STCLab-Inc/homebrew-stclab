2d582b7ee0ab603f14f2d4acc58b13c082372f7f
Ian <iamian@stclab.com>
2026-03-10 09:57:10 +0900

    Use github_private_release download strategy
    Fix 404 on private repo release asset download.

--- Formula/amaranth-check.rb
+++ Formula/amaranth-check.rb
@@ -1,7 +1,8 @@
 class AmaranthCheck < Formula
   desc "macOS menu bar app for Amaranth attendance tracking"
   homepage "https://github.com/STCLab-Inc/amaranth-check"
-  url "https://github.com/STCLab-Inc/amaranth-check/releases/download/v0.3.0/amaranth-check-0.3.0-arm64.tar.gz"
+  url "https://github.com/STCLab-Inc/amaranth-check/releases/download/v0.3.0/amaranth-check-0.3.0-arm64.tar.gz",
+      using: :github_private_release
   sha256 "eb473e6bb2db4a4523b8d374183736223e67eb678b323399f7165f223523aed2"
   version "0.3.0"
   license "Proprietary"
