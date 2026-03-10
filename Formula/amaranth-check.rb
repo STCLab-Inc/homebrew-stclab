81046ca5c2e16ad7b74e83057f1d3ebdb8906b6c
Ian <iamian@stclab.com>
2026-03-10 09:38:57 +0900

    Bump amaranth-check to 0.2.0

--- Formula/amaranth-check.rb
+++ Formula/amaranth-check.rb
@@ -2,7 +2,7 @@ class AmaranthCheck < Formula
   desc "macOS menu bar app for Amaranth attendance tracking"
   homepage "https://github.com/STCLab-Inc/amaranth-check"
   url "https://github.com/STCLab-Inc/amaranth-check.git", branch: "main"
-  version "0.1.0"
+  version "0.2.0"
   license "Proprietary"
 
   depends_on "node"
